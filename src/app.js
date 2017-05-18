1710f543d708ed4ac208bccc828a9fed3f61a897
Brennen Sanders <brennen@lightningkite.com>
2017-05-18 10:46:54 -0600

    Mountain of changes. Since JS is always used in every project, I added in react. I added in autoprefixer to get rid of all of the sass mixins needed to work for all the browsers

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/home';
+import './sass/main.scss';
+
+ReactDOM.render(
+  <Home />,
+  document.querySelector('.bs-content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/app.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(scss|css)$/,
+        include: APP_DIR,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [ // webpack 2 loader => use
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: { plugins: [require('autoprefixer')({ browsers: ['last 2 versions'] })] },
+            },
+            'sass-loader',
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new ExtractTextPlugin('main.css'),
+  ]
+};
+
+module.exports = config;
